494b04cfccbe3f22188cacb66eed575c429abc98
Kate <ek.kladova@gmail.com>
2020-05-24 16:42:19 +0100

    Updates

---
+++ precache-manifest.fb0fafc7c01fdee887278bcd7cd35fef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10632f64f69ab4d287b266abc6b87109",
+    "url": "/ekladova/react-milestone-1/index.html"
+  },
+  {
+    "revision": "85d25f9eaf31768a40b6",
+    "url": "/ekladova/react-milestone-1/static/css/main.b038944e.chunk.css"
+  },
+  {
+    "revision": "e4f181b57fffec47d868",
+    "url": "/ekladova/react-milestone-1/static/js/2.dc8bae39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ekladova/react-milestone-1/static/js/2.dc8bae39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d25f9eaf31768a40b6",
+    "url": "/ekladova/react-milestone-1/static/js/main.dddcfeda.chunk.js"
+  },
+  {
+    "revision": "3faa88a712933abf159f",
+    "url": "/ekladova/react-milestone-1/static/js/runtime-main.55aa0cee.js"
+  },
+  {
+    "revision": "3be7f2c409c7a46a14db442c0f8e4015",
+    "url": "/ekladova/react-milestone-1/static/media/6.3be7f2c4.png"
+  },
+  {
+    "revision": "b17e52674f2dba5f21d1c4bd85aa20e9",
+    "url": "/ekladova/react-milestone-1/static/media/image.b17e5267.jpg"
+  },
+  {
+    "revision": "95eba9fdca2af6211d9c190f1f3f2902",
+    "url": "/ekladova/react-milestone-1/static/media/img.95eba9fd.jpg"
+  }
+]);
